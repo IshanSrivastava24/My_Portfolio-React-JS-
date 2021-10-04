3161c6764d3b9d8e5c58651f234990a37da7dcb2
Ishan Srivastava <ishan05278@gmail.com>
2021-10-04 17:16:45 +0530

    first commit

---
+++ src/components/data/about_data.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import api from '../../assets/icons/api.svg'
+import backend from '../../assets/icons/backend.svg'
+import algo from '../../assets/icons/algo.svg'
+import computer from '../../assets/icons/computer.svg'
+import repair from '../../assets/icons/repair.svg'
+import puzzle from '../../assets/icons/puzzle.svg'
+
+
+const skills = [
+    {
+        icon: computer,
+        title: "Frontend Development",
+        about: "I can build a beautiful and scalable SPA using HTML, CSS and React.js"
+    },
+    {
+        icon: repair,
+        title: "React JS Developer",
+        about: "I can develop new user-facing features using React.js",
+    },
+    // {
+    //     icon: api,
+    //     title: "API Development",
+    //     about: ["I can develop robust REST API using", <strong key={1}> django-rest-api </strong>]
+    // },
+    {
+        icon: algo,
+        title: "Competitive Coder",
+        about: "a daily problem solver in HackerRank and Leetcode"
+    },
+    // {
+    //     icon: puzzle,
+    //     title: "UI/UX designer",
+    //     about: "minimalistic user interface designer using figma and  framer"
+    // },
+    {
+        icon: puzzle,
+        title: "React Native",
+        about: "I can build a pixel-perfect, buttery smooth UIs across both mobile platforms"
+    },
+]
+
+export default skills;
---
+++ src/components/data/resume_data.js
@@ -0,0 +1,67 @@
+import react from '../../assets/icons/react.svg'
+import python from '../../assets/icons/python.svg'
+
+const languages = [
+    {
+        icon: react,
+        name: 'React JS',
+        level: '85'
+    },
+    {
+        icon: react,
+        name: 'React Native',
+        level: '70'
+    },
+    {
+        icon: react,
+        name: 'Material UI',
+        level: '60'
+    },
+    {
+        icon: react,
+        name: 'JavaScript',
+        level: '70'
+    },
+    {
+        icon: react,
+        name: 'CSS',
+        level: '85'
+    },
+    {
+        icon: react,
+        name: 'HTML',
+        level: '80'
+    },
+    {
+        icon: react,
+        name: 'Bootstrap',
+        level: '90'
+    },
+    
+]
+
+const tools = [
+    {
+        icon: react,
+        name: 'Ubuntu',
+        level: '55'
+    },
+    {
+        icon: react,
+        name: 'Linux',
+        level: '45'
+    },
+    {
+        icon: react,
+        name: 'Windows',
+        level: '90'
+    },
+    // {
+    //     icon: react,
+    //     name: 'Framer',
+    //     level: '45'
+    // },
+
+]
+
+export { tools, languages }
